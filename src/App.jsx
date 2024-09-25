081f7fa83cbaab66cdba0f256ce7b2ec3112f25c
seedevk8s <seedevk8s@gmail.com>
2024-09-25 23:14:07 +0900

    BrowserRouter 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import Info from './Info'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,16 @@ function App() {
   return (
     <>
       <h2>App.jsx</h2>
+      <BrowserRouter>
+        <Link to="/">Home</Link> {/* <br /> */}
+        <Link to="/info">Info</Link>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Info />} />
+        </Routes> 
+      
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Info.jsx
@@ -0,0 +1,9 @@
+const Info = () => {
+  return (
+    <>
+      <h1>Info 인포.jsx</h1>
+    </>
+  )
+}
+
+export default Info
